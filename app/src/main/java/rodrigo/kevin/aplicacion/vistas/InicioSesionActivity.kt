09d6414467b5bf08cced99572804f66d9afa2b30
DevNesli <rigo_22mayo@hotmail.com>
2024-04-01 12:22:32 -0600

    Implementacion funcionalidades
    Creación de funcionalidades de la interfaz de iniciar sesión.

--- app/src/main/java/rodrigo/kevin/aplicacion/vistas/InicioSesionActivity.kt
+++ app/src/main/java/rodrigo/kevin/aplicacion/vistas/InicioSesionActivity.kt
@@ -2,14 +2,28 @@ package rodrigo.kevin.aplicacion.vistas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import rodrigo.kevin.R
 
 class InicioSesionActivity : AppCompatActivity() {
+
+    lateinit var btnIngresar: Button
+    lateinit var tvRegistrarse: TextView
+    lateinit var tvRecuperarContra: TextView
+    lateinit var txtCorreo: EditText
+    lateinit var txtContra: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inicio_sesion)
 
+        this.btnIngresar = findViewById(R.id.btnIngresar)
+        this.tvRegistrarse = findViewById(R.id.lblRegistrarse)
+        this.tvRecuperarContra = findViewById(R.id.lblOlvidoContra)
+        this.txtCorreo = findViewById(R.id.txtCorreo)
+        this.txtContra = findViewById(R.id.txtContra)
 
     }
 
